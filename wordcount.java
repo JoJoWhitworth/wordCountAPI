2273cf18123fc395ac1e4306b959a9136f66f6de
JoJoWhitworth <55980234+JoJoWhitworth@users.noreply.github.com>
2021-02-26 11:50:33 +0000

    Update wordcount.java

--- wordcount.java
+++ wordcount.java
@@ -1,23 +1,26 @@
-package com.joewhitworth.wordcountapi;
+package com.wordCounter.demo;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
 import java.util.*;
 import java.lang.Object;
 import java.text.DecimalFormat;
 import java.util.stream.Collectors;
 
-public class wordcount {
-	static File input = new File(
-			"C:\\Users\\J-del\\eclipse-workspace\\WordCountAPI\\src\\com\\joewhitworth\\wordcountapi\\input.txt");
+@SpringBootApplication
+@RestController
+public class WordCounterApplication {
+
+	static File input = new File("[FILE INPUT]");
 
 	// Regex string that removes special characters with the exception of: &, /,
 	// whitespace
 	static String removeSpecialCharacters = "[^\\w\\s\\/\\&]|_";
 
-	static void wordCount() throws IOException {
+	public static void wordAndCharCount() throws IOException{
 		FileReader fr = new FileReader(input);
 		BufferedReader br = new BufferedReader(fr);
 
@@ -26,14 +29,12 @@ public class wordcount {
 		int wordCount = 0;
 		int charCount = 0;
 		double average = 0;
-
-		while ((line = br.readLine()) != null) // While loop for counting characters and number of words
-		{
+		// While loop for counting characters and number of words
+		while ((line = br.readLine()) != null){
 			line = line.replaceAll(removeSpecialCharacters, "");
 
 			String[] words = line.split(" "); // Determine what separates the words, in this instance it will be spaces.
-			for (int i = 0; i < words.length; i++) // This counts the words in the text file
-			{
+			for (int i = 0; i < words.length; i++){ // This counts the words in the text file
 				if (words[i].length() == 0) {
 					continue;
 				}
@@ -41,22 +42,23 @@ public class wordcount {
 			}
 			line = line.replaceAll("\\s", ""); // Removes whitespaces
 			String[] characters = line.split("");
-			for (int j = 0; j < characters.length; j++) // This counts the characters in the text file
-			{
+			for (int j = 0; j < characters.length; j++) {
 				charCount++;
 			}
 		}
 		DecimalFormat df2 = new DecimalFormat("#.###"); // Formats the decimal to be 3DP
 		average = (double) charCount / wordCount; // Calculation for the average word length
+
 		System.out.println("Word Count = " + wordCount);
 		System.out.println("Average word length = " + df2.format(average)); // 3 decimal place.
+
 		fr.close();
 		br.close();
 	}
 
-	static void numberOfWords() throws IOException {
+	public static void numberOfWords() throws IOException{
 		Map<Integer, Integer> lengthCount = new HashMap<Integer, Integer>();
-		try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(input))){
 
 			StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
@@ -98,14 +100,16 @@ public class wordcount {
 		String maxWordLengths = lengthCount.entrySet()
 				.stream()
 				.filter(entry -> finalValue == entry
-				.getValue())
-				.map(e -> e.getKey().toString()).collect(Collectors.joining(", "));
+						.getValue())
+				.map(e -> e.getKey().toString())
+				.collect(Collectors.joining(", "));
 
 		System.out.println("The most frequently occurring word length is " + maxValue + ", for word lengths of " + maxWordLengths);
 	}
 
 	public static void main(String[] args) throws IOException {
-		wordCount();
+		SpringApplication.run(WordCounterApplication.class, args);
+		wordAndCharCount();
 		numberOfWords();
 	}
 }
